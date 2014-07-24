224dee2a5c96c1fddb43c8cdb545c096e37b52c4
Yoonsung <lvev9925@naver.com>
2014-07-24 09:35:04 +0900

    Press Toggle And Complete todo job Event Complete

---
+++ index.js
@@ -0,0 +1,61 @@
+var oUtil = {
+	isUndefinedOrNull: function(value) {
+		return (value === undefined || value === null);
+	}
+};
+
+var oTodo = {
+	eList: null,
+	eInput: null,
+	eTodoTemplate : null,
+
+	eventHandler: function() {
+		this.eInput.addEventListener("keydown", function(e) {
+
+    		//if Press Enter Key
+    		if (e.keyCode === 13) {
+    			this.add();
+    			this.eInput.value = "";
+    		}
+    	}.bind(this));
+
+    	this.eList.addEventListener("click", this.clickList.bind(this));
+	},
+
+	init: function() {
+		this.eList = document.getElementById("todo-list");
+		this.eInput = document.getElementById("new-todo");
+		this.eTodoTemplate = document.getElementById("template-todo");
+
+		this.eventHandler();
+	},
+
+	add: function() {
+
+		var sInput = this.eInput.value;
+		if (oUtil.isUndefinedOrNull(sInput))
+			return;
+
+		var sTodo = this.eTodoTemplate.innerText;
+		this.eList.insertAdjacentHTML('afterbegin', sTodo.replace(/{}/, sInput));
+	},
+
+	clickList: function(e) {
+
+		//e.target = trigger element
+		//e.currentTarget =  event binding element
+		var eClicked = e.target;
+		console.log(eClicked);
+
+		//Press Complete toggle
+		if (eClicked.tagName == "INPUT") {
+			var eTargetLi = eClicked.parentNode.parentNode;
+			
+			this.complete(eTargetLi);	
+		}
+	},
+
+	complete: function(eClickedLi) {
+		eClickedLi.className = "completed";
+	}
+};
--- test.js
+++ test.js
@@ -7,3 +7,55 @@
 //==========Given==========
 //==========When==========
 //==========Then==========
+
+test("TODO 생성테스트", function() {
+
+	//==========Initialize==========
+	oTodo.init();
+	var eTodoList = document.getElementById("todo-list");
+	var eFakeList = document.getElementById("fake-todo-list");
+	
+	//==========Given==========
+	var sExpectedInnerHTML = "<li>"
+                        + "<div class=\"view\">"
+                        + "<input class=\"toggle\" type=\"checkbox\">"
+                        + "<label></label>"
+                        + "<button class=\"destroy\"></button>"
+                        + "</div>"
+                        + "</li>";
+
+	//==========When==========
+	var inputValue = "test"
+
+	oTodo.eInput.value = inputValue;
+	oTodo.add();
+	eFakeList.insertAdjacentHTML('afterbegin', sExpectedInnerHTML);
+	eFakeList.querySelector('label').value= inputValue;
+
+	//==========Then==========	
+	var resultLi = eTodoList.querySelector("li");
+	var fakeResultLi = eFakeList.querySelector("li");
+
+	equal(resultLi.outerHTML, fakeResultLi.outerHTML);
+});
+
+test("TODO 완료테스트", function() {
+	//==========Initialize==========
+	oTodo.init();
+	var eToggle;
+	var eLiTarget;
+	
+	//==========Given==========
+	oTodo.add();
+	eToggle = document.querySelector(".toggle");
+	console.log(eToggle);
+
+	eLiTarget = document.querySelector("#todo-list").children[0];
+	equal(eLiTarget.className, "");
+	
+	//==========When==========
+	fireEvent(eToggle,"click");
+
+	//==========Then==========
+	equal(eLiTarget.className, "completed");
+});
