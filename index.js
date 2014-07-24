0ea6e27f83a6ae6fd46f7c1b2fa48d68669b5493
Yoonsung <lvev9925@naver.com>
2014-07-24 16:15:53 +0900

    Add Animation on create new todo And destroy

--- index.js
+++ index.js
@@ -12,35 +12,46 @@ var oTodo = {
 	eventHandler: function() {
 		this.eInput.addEventListener("keydown", function(e) {
 
-    		//if Press Enter Key
-    		if (e.keyCode === 13) {
-    			this.add();
-    			this.eInput.value = "";
+			//if Press Enter Key
+			if (e.keyCode === 13) {
+				this.add();
+				this.eInput.value = "";
     		}
     	}.bind(this));
 
-    	this.eList.addEventListener("click", this.clickList.bind(this));
+		this.eList.addEventListener("click", this.clickList.bind(this));
+
+		/*
+		TODO Browser Detection
+		TODO Animation Test
+		https://developer.mozilla.org/en-US/docs/Web/Events/transitionend
+    	*/
+    	this.eList.addEventListener("webkitTransitionEnd", this.animationEnd.bind(this));
 	},
 
-	init: function() {
-		this.eList = document.getElementById("todo-list");
-		this.eInput = document.getElementById("new-todo");
-		this.eTodoTemplate = document.getElementById("template-todo");
+    init: function() {
+    	this.eList = document.getElementById("todo-list");
+    	this.eInput = document.getElementById("new-todo");
+    	this.eTodoTemplate = document.getElementById("template-todo");
 
-		this.eventHandler();
-	},
+    	this.eventHandler();
+    },
 
-	add: function() {
+    add: function() {
 
-		var sInput = this.eInput.value;
-		if (oUtil.isUndefinedOrNull(sInput))
-			return;
+    	var sInput = this.eInput.value;
+    	if (oUtil.isUndefinedOrNull(sInput))
+    		return;
 
-		var sTodo = this.eTodoTemplate.innerText;
-		this.eList.insertAdjacentHTML('afterbegin', sTodo.replace(/{}/, sInput));
-	},
+    	var sTodo = this.eTodoTemplate.innerText;
+    	this.eList.insertAdjacentHTML('afterbegin', sTodo.replace(/{}/, sInput));
+    	
+    	this.eList.children[0].style.opacity = 0;
+    	this.eList.offsetHeight;
+    	this.eList.children[0].style.opacity = 1;
+    },
 
-	clickList: function(e) {
+    clickList: function(e) {
 
 		//e.target = trigger element
 		//e.currentTarget =  event binding element
@@ -59,7 +70,21 @@ var oTodo = {
 		//Press Destroy Button
 		} else if (eClicked.tagName == "BUTTON") {
 			var eTargetLi = eClicked.parentNode.parentNode;
-			eTargetLi.parentNode.removeChild(eTargetLi);
+			eTargetLi.style.opacity = 0;
+			
+		}
+	},
+
+	animationEnd: function(e) {
+		var eAnimated = e.target;
+
+		if (eAnimated.tagName !== "LI")
+			return;
+
+		var opacity = eAnimated.style.opacity;
+		
+		if (opacity == 0) {
+			console.log(this.eList.removeChild(eAnimated));
 		}
 	}
 };
